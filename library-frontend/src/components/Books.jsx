d71c4580d48594682a71aeb52dbc1daae99f6059
stm32dev <stm32dev@outlook.com>
2025-05-17 19:12:53 +0800

    Complete exercise 8.19
    Implement genre filter in book page using react

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = (props) => {
+
+  const [filter, setFilter] = useState('')
 
   const result = useQuery(ALL_BOOKS)
 
@@ -9,12 +12,35 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
+  console.log(result.data.allBooks);
+
   const books = result.data.allBooks
+
+  const booksToDisplay = filter
+    ? result.data.allBooks.filter(book => {
+      if (book.genres.includes(filter)) {
+        return true
+      }
+      return false
+    })
+    : books
+
+  console.log(booksToDisplay);
+
+  const genreList = []
+
+  books.forEach(book => {
+    book.genres.forEach(genre => {
+      if (!genreList.includes(genre)) {
+        genreList.push(genre)
+      }
+    })
+  })
 
   return (
     <div>
       <h2>books</h2>
-
+      {filter ? <div>in genre <em>{filter}</em></div> : <></>}
       <table>
         <tbody>
           <tr>
@@ -22,7 +48,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {booksToDisplay.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +57,22 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <br />
+      <div>
+        <button onClick={() => setFilter('')}>clear genre</button>
+        {genreList.map(genre => {
+          return (
+            <button
+              key={genre}
+              onClick={({ target }) => setFilter(target.textContent)}
+            >
+              {genre}
+            </button>
+          )
+        })}
+
+      </div>
+
     </div>
   )
 }
